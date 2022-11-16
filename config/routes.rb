02f921f5d477f8b57e96db294a701095bd0e71ac
william passera <passeraw@gmail.com>
2022-11-16 16:22:25 +0100

    yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # get "restaurants/:id", to: "restaurants#show"
+  # post "restaurants", to: "restaurants#create"
+
+  resources :restaurants, only: [:index, :show, :new, :create, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.new(name: "Le Café blanc", address: "75001 Paris", category: "french")
+Restaurant.new(name: "Fries", address: "Brussels", category: "belgian")
+Restaurant.new(name: "Carmine's", address: "New York", category: "italian")
+Restaurant.new(name: "Neko Ramen", address: "750010 Paris", category: "japanese")
+Restaurant.new(name: "Beijing", address: "London", category: "chinese")
